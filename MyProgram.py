1d15fe7bddf7b0400281ff7a611990e4d21a2c2e
luckyalishakil <luckyalishakil@gmail.com>
2024-11-22 15:59:51 +0000

    Update MyProgram.py

--- MyProgram.py
+++ MyProgram.py
@@ -10,8 +10,11 @@ def multiply(first_number, second_number):
 
 def divide(first_number, second_number):
     return first_number / second_number
-    
 
+def Display():
+    print("Printing...)
+    
+print("Results...")
 print(add(-5,7))
 print(subtract(-5,7))
 print(multiply(-5,7))
