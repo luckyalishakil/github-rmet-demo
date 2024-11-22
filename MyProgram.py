12fa15d86147a7410d3761d95aabe4c428f088d8
luckyalishakil <luckyalishakil@gmail.com>
2024-11-22 15:55:51 +0000

    Add files via upload

---
+++ MyProgram.py
@@ -0,0 +1,17 @@
+
+def add(first_number, second_number):
+    return first_number + second_number
+
+def subtract(first_number, second_number):
+    return first_number - second_number
+
+def multiply(first_number, second_number):
+    return first_number * second_number
+
+def divide(first_number, second_number):
+    return first_number / second_number
+    
+
+print(add(-5,7))
+print(subtract(-5,7))
+print(multiply(-5,7))
